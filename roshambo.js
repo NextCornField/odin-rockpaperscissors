a6f645c3030831af0d2a3e5385b3e36badabebf6
Mckenzie <connell.mckenzie@gmail.com>
2022-04-30 12:39:37 -0500

    Fix conditionals and add html vieing file
    Replace all = with proper == in conditional statements

--- roshambo.js
+++ roshambo.js
@@ -11,47 +11,45 @@ function playRound(playerSelection, computerSelection = "Rock") {
     var result = 0; // 0 for tie, 1 for user win, -1 for computer win
     var resultMsg = ""; // empty text var for result message
 
-    if (playerSelection = "rock") {
+    if (playerSelection == "rock") {
         //rock ties rock
-        if (computerSelection = "rock") {
+        if (computerSelection == "rock") {
             result = 0; // 0 is for tie
         }
         //rock loses to paper
-        else if (computerSelection = "paper") {
+        else if (computerSelection == "paper") {
             result = -1; //-1 means computer wins
         }
         //rock beats scissors
-        else if (computerSelection = "scissors") {
+        else if (computerSelection == "scissors") {
             result = 1; //1 means user win
         }
     }
-
-    if (playerSelection = "paper") {
+    else if (playerSelection == "paper") {
         //paper ties paper
-        if (computerSelection = "paper") {
+        if (computerSelection == "paper") {
             result = 0; // 0 is for tie
         }
         //paper loses to scissors
-        else if (computerSelection = "scissors") {
+        else if (computerSelection == "scissors") {
             result = -1; //-1 means computer wins
         }
         //paper beats rock
-        else if (computerSelection = "rock") {
+        else if (computerSelection == "rock") {
             result = 1; //1 means user win
         }
     }
-
-    if (playerSelection = "scissors") {
+    else if (playerSelection == "scissors") {
         //scissors ties scissors
-        if (computerSelection = "scissors") {
+        if (computerSelection == "scissors") {
             result = 0; // 0 is for tie
         }
         //scissors loses to rock
-        else if (computerSelection = "rock") {
+        else if (computerSelection == "rock") {
             result = -1; //-1 means computer wins
         }
         //scissors beats paper
-        else if (computerSelection = "paper") {
+        else if (computerSelection == "paper") {
             result = 1; //1 means user win
         }
     }
